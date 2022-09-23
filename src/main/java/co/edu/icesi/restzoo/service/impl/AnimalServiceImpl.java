96d8a1a07e4d585c57832a277adbc346a40d941c
zacwastaken <doicirohangnan@gmail.com>
2022-09-23 17:04:46 -0500

    fix: implemented service properly

--- src/main/java/co/edu/icesi/restzoo/service/impl/AnimalServiceImpl.java
+++ src/main/java/co/edu/icesi/restzoo/service/impl/AnimalServiceImpl.java
@@ -1,29 +1,32 @@
 package co.edu.icesi.restzoo.service.impl;
 
 import co.edu.icesi.restzoo.model.Animal;
+import co.edu.icesi.restzoo.repository.ZooRepository;
 import co.edu.icesi.restzoo.service.AnimalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 @Service
 @RequiredArgsConstructor
 public class AnimalServiceImpl implements AnimalService {
 
+    public final ZooRepository zooRepository;
+
     @Override
-    public Animal getAnimal(UUID animalId) {
-        return null;
-    }
+    public Animal getAnimal(UUID animalId) { return zooRepository.findById(animalId).orElse(null); }
 
     @Override
     public Animal createAnimal(Animal animal) {
-        return null;
+        return zooRepository.save(animal);
     }
 
     @Override
     public List<Animal> getAnimals() {
-        return null;
+        return StreamSupport.stream(zooRepository.findAll().spliterator(),false).collect(Collectors.toList());
     }
 }
