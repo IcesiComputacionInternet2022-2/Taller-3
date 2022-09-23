b2a0129a99dbf4096c1d1438a561f46f764ecc8f
zacwastaken <doicirohangnan@gmail.com>
2022-09-23 07:50:58 -0500

    Initial commit

---
+++ src/main/java/co/edu/icesi/restzoo/model/Animal.java
@@ -0,0 +1,40 @@
+package co.edu.icesi.restzoo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Table(name = "ANIMAL")
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Animal {
+
+    @Id
+    @Type(type="org.hibernate.type.UUIDCharType")
+    private UUID id;
+    private String name;
+    private char sex;
+    private float weight;
+    private int age;
+    private float height;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime arrivalDate;
+
+    @PrePersist
+    public void generateId(){
+        this.id = UUID.randomUUID();
+    }
+}
---
+++ src/main/java/co/edu/icesi/restzoo/repository/ZooRepository.java
@@ -0,0 +1,11 @@
+package co.edu.icesi.restzoo.repository;
+
+import co.edu.icesi.restzoo.model.Animal;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ZooRepository extends CrudRepository<Animal, UUID> {
+}
