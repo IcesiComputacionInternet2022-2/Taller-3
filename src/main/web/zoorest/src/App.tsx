d8f0c01c39ce072c93fc3343a9ac14e93d1ff46b
zacwastaken <doicirohangnan@gmail.com>
2022-11-11 14:40:18 -0500

    added tailwind config files to project

--- src/main/web/zoorest/src/App.tsx
+++ src/main/web/zoorest/src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [animals, setAnimals] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+
+    fetch("animals").then(res => res.json()).then(data => {
+      setAnimals(data);
+      setLoading(false);
+    })
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
